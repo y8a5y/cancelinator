ba5dd16c1c1fb327a3d7697ef4897db6d12ff010
y8a5y <code.github.y8a5y@passmail.net>
2025-10-01 17:53:18 +0200

    image to bytes works

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-import os, re, dotenv, discord
+import os, re, dotenv, discord, requests
+from PIL import Image
+from io import BytesIO
 from discord import option, ApplicationContext as Context, Message
 
 bot = discord.Bot()
@@ -7,7 +9,15 @@ bot = discord.Bot()
 @discord.option("target", description="l'accusé·e", type=discord.SlashCommandOptionType.user)
 @discord.option("reason", description="la raison du cancel", type=discord.SlashCommandOptionType.string)
 async def cancel(ctx, target, reason):
-    await ctx.respond(f"target: {target.avatar}, reason={reason}")
+    img = Image.open(requests.get(target.avatar, stream=True).raw)
+    img = img.resize((256,256))
+    img = img.transpose(Image.FLIP_LEFT_RIGHT)
+
+    bytes = BytesIO()
+    img.save(bytes, format="PNG")
+    bytes.seek(0)
+
+    await ctx.respond(f"raison : {reason}, type: {type(bytes)}", file=discord.File(bytes, filename="target.png"))
 
 @bot.event
 async def on_ready():
