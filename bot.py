f7c1c4dc06fa4302404654bdff6bd3a3c04602a3
y8a5y <code.github.y8a5y@passmail.net>
2025-10-03 14:22:46 +0200

    v1 should be good to go

--- bot.py
+++ bot.py
@@ -1,27 +1,53 @@
 import os, re, dotenv, discord, requests
-from PIL import Image
+from PIL import Image, ImageOps
 from io import BytesIO
 from discord import option, ApplicationContext as Context, Message
 
 bot = discord.Bot()
+overlay = Image.open(r"assets/cancel.png").convert("RGBA")
 
-@bot.command()
-@discord.option("target", description="l'accusé·e", type=discord.SlashCommandOptionType.user)
-@discord.option("reason", description="la raison du cancel", type=discord.SlashCommandOptionType.string)
+@bot.slash_command(
+    name="cancel",
+    description="Parce que même la liberté d'expression a ses limites."
+)
+@discord.option(
+    name="qui",
+    parameter_name="target",
+    description="l'accusé·e",
+    type=discord.SlashCommandOptionType.user
+)
+@discord.option(
+    name="pourquoi",
+    parameter_name="reason",
+    description="la raison (totalement justifiée) du cancel (max 300 caractères)",
+    type=str,
+    max_length=300
+)
 async def cancel(ctx, target, reason):
-    img = Image.open(requests.get(target.avatar, stream=True).raw)
-    img = img.resize((256,256))
-    img = img.transpose(Image.FLIP_LEFT_RIGHT)
+    avatar = Image.open(requests.get(target.avatar, stream=True).raw)
 
+    # L mode converts to greyscale
+    # then back to RGBA so overlay doesn't get greyscaled
+    avatar = avatar \
+        .convert("L") \
+        .convert("RGBA") \
+        .resize((256, 256))
+
+    avatar.paste(overlay, mask=overlay)
+
+    # saving to bytes
     bytes = BytesIO()
-    img.save(bytes, format="PNG")
+    avatar.save(bytes, format="PNG")
     bytes.seek(0)
 
-    await ctx.respond(f"raison : {reason}, type: {type(bytes)}", file=discord.File(bytes, filename="target.png"))
+    await ctx.respond(
+        f"<@{ctx.author.id}> a cancel <@{target.id}>.\nRaison invoquée : {reason}",
+        file=discord.File(bytes, filename="canceled.png")
+    )
 
 @bot.event
 async def on_ready():
     print(f"logged in as {bot.user}")
 
-dotenv.load_dotenv()
+dotenv.load_dotenv() # loads .env file, if any
 bot.run(os.getenv("DISCORD_TOKEN"))
